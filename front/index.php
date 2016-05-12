f628f012e626a0e072e0f2e4f1f46328c345c9c0
mikearchie254 <mondieki9@gmail.com>
2016-05-12 15:27:50 +0300

    CSS referencing in "index.php"

--- front/index.php
+++ front/index.php
@@ -10,11 +10,14 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <head>
 <title>Botik Online Clothing Store</title>
 <link href="../css/front/bootstrap.css" rel='stylesheet' type='text/css' />
+
 <!-- jQuery (necessary JavaScript plugins) -->
 <script type='text/javascript' src="../js/front/jquery-1.11.1.min.js"></script>
+
 <!-- Custom Theme files -->
 <link href="../css/front/style.css" rel='stylesheet' type='text/css' />
 <!-- Custom Theme files -->
+
 <!--//theme-style-->
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -23,14 +26,16 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
 <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900' rel='stylesheet' type='text/css'>
 <link href='http://fonts.googleapis.com/css?family=Playfair+Display:400,700,900' rel='stylesheet' type='text/css'>
+
 <!-- start menu -->
-<link href="../assets/css/front/megamenu.css" rel="stylesheet" type="text/css" media="all" />
+<link href="../css/front/megamenu.css" rel="stylesheet" type="text/css" media="all" />
 <script type="text/javascript" src="../js/front/megamenu.js"></script>
 <script>$(document).ready(function(){$(".megamenu").megamenu();});</script>
 <script src="../js/front/menu_jquery.js"></script>
 <script src="../js/front/simpleCart.min.js"> </script>
 </head>
 <body>
+
 <!-- header_top -->
 <div class="top_bg">
 	<div class="container">
@@ -49,6 +54,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 		</div>
 	</div>
 </div>
+
 <!-- header -->
 <div class="header_bg">
 <div class="container">
@@ -59,6 +65,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 				<img src="../images/logo.jpg" class="img-responsive" alt=""/>
 			 </a>
 		</div>
+
 		<!-- start header_right -->
 		<div class="header_right">
 			<div class="rgt-bottom">
