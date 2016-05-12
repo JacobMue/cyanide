b67be2d7b3b546ce132461500a31f7c5824d6859
mikearchie254 <mondieki9@gmail.com>
2016-05-12 16:14:21 +0300

    front/index/php CSS referencing

--- front/index.php
+++ front/index.php
@@ -8,13 +8,16 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <!DOCTYPE HTML>
 <html>
 <head>
-<title>Gretong a Ecommerce Category Flat Bootstarp Responsive Website Template | Home :: w3layouts</title>
+<title>Botik Online Clothing Store</title>
 <link href="../css/front/bootstrap.css" rel='stylesheet' type='text/css' />
+
 <!-- jQuery (necessary JavaScript plugins) -->
 <script type='text/javascript' src="../js/front/jquery-1.11.1.min.js"></script>
+
 <!-- Custom Theme files -->
 <link href="../css/front/style.css" rel='stylesheet' type='text/css' />
 <!-- Custom Theme files -->
+
 <!--//theme-style-->
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -23,6 +26,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
 <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900' rel='stylesheet' type='text/css'>
 <link href='http://fonts.googleapis.com/css?family=Playfair+Display:400,700,900' rel='stylesheet' type='text/css'>
+
 <!-- start menu -->
 <link href="../css/front/megamenu.css" rel="stylesheet" type="text/css" media="all" />
 <script type="text/javascript" src="../js/front/megamenu.js"></script>
@@ -31,6 +35,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 <script src="../js/front/simpleCart.min.js"> </script>
 </head>
 <body>
+
 <!-- header_top -->
 <div class="top_bg">
 	<div class="container">
@@ -49,6 +54,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 		</div>
 	</div>
 </div>
+
 <!-- header -->
 <div class="header_bg">
 <div class="container">
@@ -56,8 +62,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 	<div class="head-t">
 		<div class="logo">
 			<a href="index.html">
-			<h2>InstaClothes</h2>
-			<!-- <img src=""../images/logo.png" class="img-responsive" alt=""/> -->
+				<img src="../images/logo.jpg" class="img-responsive" alt=""/>
 			 </a>
 		</div>
 		<!-- start header_right -->
