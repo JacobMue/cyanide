5144c30cc47a6e78ef7fbe33443b38b063250520
mikearchie254 <mondieki9@gmail.com>
2016-05-13 10:09:13 +0300

    css referencing

--- front/index.php
+++ front/index.php
@@ -133,7 +133,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 				</div>
 			<div class="cart box_1">
 				<a href="checkout.html">
-					<h3> <span class="simpleCart_total">KSH 0.00</span> (<span id="simpleCart_quantity" class="simpleCart_quantity">0</span> items)<img src=""../images/bag.png" alt=""></h3>
+					<h3> <span class="simpleCart_total">KSH 0.00</span> (<span id="simpleCart_quantity" class="simpleCart_quantity">0</span> items)<img src="../images/bag.png" alt=""></h3>
 				</a>	
 				<p><a href="javascript:;" class="simpleCart_empty">(empty card)</a></p>
 				<div class="clearfix"> </div>
@@ -803,7 +803,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 	<div class="container">
 		<div class="arriv-top">
 			<div class="col-md-6 arriv-left">
-				<img src=""../images/1.jpg" class="img-responsive" alt="">
+				<img src="../images/1.jpg" class="img-responsive" alt="">
 				<div class="arriv-info">
 					<h3>NEW ARRIVALS</h3>
 					<p>REVIVE YOUR WARDROBE WITH CHIC KNITS</p>
@@ -813,7 +813,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 				</div>
 			</div>
 			<div class="col-md-6 arriv-right">
-				<img src=""../images/2.jpg" class="img-responsive" alt="">
+				<img src="../images/2.jpg" class="img-responsive" alt="">
 				<div class="arriv-info">
 					<h3>TUXEDO</h3>
 					<p>REVIVE YOUR WARDROBE WITH CHIC KNITS</p>
@@ -826,7 +826,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 		</div>
 		<div class="arriv-bottm">
 			<div class="col-md-8 arriv-left1">
-				<img src=""../images/3.jpg" class="img-responsive" alt="">
+				<img src="../images/3.jpg" class="img-responsive" alt="">
 				<div class="arriv-info1">
 					<h3>SWEATER</h3>
 					<p>REVIVE YOUR WARDROBE WITH CHIC KNITS</p>
@@ -836,7 +836,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 				</div>
 			</div>
 			<div class="col-md-4 arriv-right1">
-				<img src=""../images/4.jpg" class="img-responsive" alt="">
+				<img src="../images/4.jpg" class="img-responsive" alt="">
 				<div class="arriv-info2">
 					<a href="details.html"><h3>Trekking Shoes<i class="ars"></i></h3></a>
 				</div>
@@ -845,13 +845,13 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 		</div>
 		<div class="arriv-las">
 			<div class="col-md-4 arriv-left2">
-				<img src=""../images/5.jpg" class="img-responsive" alt="">
+				<img src="../images/5.jpg" class="img-responsive" alt="">
 				<div class="arriv-info2">
 					<a href="details.html"><h3>Casual Glasses<i class="ars"></i></h3></a>
 				</div>
 			</div>
 			<div class="col-md-4 arriv-middle">
-				<img src=""../images/6.jpg" class="img-responsive" alt="">
+				<img src="../images/6.jpg" class="img-responsive" alt="">
 				<div class="arriv-info3">
 					<h3>FRESH LOOK T-SHIRT</h3>
 					<div class="crt-btn">
@@ -860,7 +860,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 				</div>
 			</div>
 			<div class="col-md-4 arriv-right2">
-				<img src=""../images/7.jpg" class="img-responsive" alt="">
+				<img src="../images/7.jpg" class="img-responsive" alt="">
 				<div class="arriv-info2">
 					<a href="details.html"><h3>Elegant Watches<i class="ars"></i></h3></a>
 				</div>
@@ -875,7 +875,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 		<div class="specia-top">
 			<ul class="grid_2">
 		<li>
-				<a href="details.html"><img src=""../images/8.jpg" class="img-responsive" alt=""></a>
+				<a href="details.html"><img src="../images/8.jpg" class="img-responsive" alt=""></a>
 				<div class="special-info grid_1 simpleCart_shelfItem">
 					<h5>Lorem ipsum dolor</h5>
 					<div class="item_add"><span class="item_price"><h6>ONLY $40.00</h6></span></div>
@@ -883,7 +883,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 				</div>
 		</li>
 		<li>
-				<a href="details.html"><img src=""../images/9.jpg" class="img-responsive" alt=""></a>
+				<a href="details.html"><img src="../images/9.jpg" class="img-responsive" alt=""></a>
 				<div class="special-info grid_1 simpleCart_shelfItem">
 					<h5>Consectetur adipis</h5>
 					<div class="item_add"><span class="item_price"><h6>ONLY $60.00</h6></span></div>
@@ -891,7 +891,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 			</div>
 		</li>
 		<li>
-				<a href="details.html"><img src=""../images/10.jpg" class="img-responsive" alt=""></a>
+				<a href="details.html"><img src="../images/10.jpg" class="img-responsive" alt=""></a>
 				<div class="special-info grid_1 simpleCart_shelfItem">
 					<h5>Commodo consequat</h5>
 					<div class="item_add"><span class="item_price"><h6>ONLY $14.00</h6></span></div>
@@ -899,7 +899,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 			</div>
 		</li>
 		<li>
-				<a href="details.html"><img src=""../images/11.jpg" class="img-responsive" alt=""></a>
+				<a href="details.html"><img src="../images/11.jpg" class="img-responsive" alt=""></a>
 				<div class="special-info grid_1 simpleCart_shelfItem">
 					<h5>Voluptate velit</h5>
 					<div class="item_add"><span class="item_price"><h6>ONLY $37.00</h6></span></div>
