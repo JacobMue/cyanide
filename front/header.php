338293992c9bfdf84764a375db8b955329121324
mikearchie254 <mondieki9@gmail.com>
2016-05-13 11:34:35 +0300

    CSS referencing

---
+++ front/header.php
@@ -0,0 +1,116 @@
+<div class="top_bg">
+	<div class="container">
+		<div class="header_top">
+			<div class="top_right">
+				<ul>
+					<li><a href="#">help</a></li>|
+					<li><a href="contact.html">Contact</a></li>|
+					<li><a href="#">Delivery information</a></li>
+				</ul>
+			</div>
+			<div class="top_left">
+				<h2><span></span> Call ME : +254 702 381 228</h2>
+			</div>
+				<div class="clearfix"> </div>
+		</div>
+	</div>
+</div>
+
+<!-- header -->
+<div class="header_bg">
+<div class="container">
+	<div class="header">
+	<div class="head-t">
+		<div class="logo">
+			<a href="index.html">
+				<img src="../images/logo.jpg" class="img-responsive" alt=""/>
+			 </a>
+		</div>
+		<!-- start header_right -->
+		<div class="header_right">
+			<div class="rgt-bottom">
+				<div class="log">
+					<div class="login" >
+						<div id="loginContainer"><a href="#" id="loginButton"><span>Login</span></a>
+						    <div id="loginBox">                
+						        <form id="loginForm" method="POST" action="index.php">
+						                <fieldset id="body">
+						                	<fieldset>
+						                          <label for="email">Email Address</label>
+						                          <input type="text" name="email" id="email">
+						                    </fieldset>
+						                    <fieldset>
+						                            <label for="password">Password</label>
+						                            <input type="password" name="password" id="password">
+						                     </fieldset>
+						                    <input type="submit" id="login" name="Submit"value="Sign in">
+						            	</fieldset>
+						            <span><a href="#">Forgot your password?</a></span>
+								</form>
+		<?php
+			$con=mysqli_connect("localhost","root","","cyanide") or die("NO server and database");
+			//mysqli_select_db("cyanide");
+
+			if ( isset( $_POST['Submit'] ) ) {
+			$email=$_POST['email'];
+			$password=$_POST['password'];
+			$password=md5($password);
+			$query="SELECT * FROM Administrators WHERE `Email`='$email'";
+			$result=mysqli_query($con,$query);
+			$row=mysqli_num_rows($result);
+
+			if($row==1){
+				while($re=mysqli_fetch_assoc($result)){
+					$dbpass=$re['Password'];
+					$fname=$re['FirstName'];
+					$lname=$re['LastName'];
+					$image=$re['image'];
+				}
+				if($dbpass==$password){
+					$_SESSION['firstName']=$fname;
+					$_SESSION['lastName']=$lname;
+					$_SESSION['image']=$image;
+					header('Location:../back/index.php');
+				}else{
+					echo "<script>";
+					echo "alert('Wrong Password')";
+					echo"</script>";
+				}
+			}else{
+				echo "<script>";
+				echo "alert('Unknown Email Address ')";
+				echo"</script>";
+			}
+			mysqli_close($con);
+			//close the isset
+			}
+		?>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="reg">
+					<a href="register.html">REGISTER</a>
+				</div>
+			<div class="cart box_1">
+				<a href="checkout.html">
+					<h3> <span class="simpleCart_total">KSH 0.00</span> (<span id="simpleCart_quantity" class="simpleCart_quantity">0</span> items)<img src="../images/bag.png" alt=""></h3>
+				</a>	
+				<p><a href="javascript:;" class="simpleCart_empty">(empty card)</a></p>
+				<div class="clearfix"> </div>
+			</div>
+			<div class="create_btn">
+				<a href="checkout.html">CHECKOUT</a>
+			</div>
+			<div class="clearfix"> </div>
+		</div>
+		<div class="search">
+		    <form>
+		    	<input type="text" value="" placeholder="search...">
+				<input type="submit" value="">
+			</form>
+		</div>
+		<div class="clearfix"> </div>
+		</div>
+		<div class="clearfix"> </div>
+	</div>
