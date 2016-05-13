92f03228c5114d4704b5e560c549dbc5e9d2e921
JacobMue <muejacob@gmail.com>
2016-05-13 04:58:58 +0300

    Update of the view layouts

--- back/men.php
+++ back/men.php
@@ -1,7 +1,69 @@
 <?php  include'./iframeImporter.php';  ?>
 
-<div>
-	
+<div id="ProductDisplay">
+	<img src="../images/1.jpg" class="img-rounded">
+	<button class="btn btn-primary">View</button>
+	<button class="btn btn-info">Modify</button>
+</div><div id="ProductDisplay">
+	<img src="../images/1.jpg" class="img-rounded">
+	<button class="btn btn-primary">View</button>
+	<button class="btn btn-info">Modify</button>
+</div><div id="ProductDisplay">
+	<img src="../images/1.jpg" class="img-rounded">
+	<button class="btn btn-primary">View</button>
+	<button class="btn btn-info">Modify</button>
+</div><div id="ProductDisplay">
+	<img src="../images/1.jpg" class="img-rounded">
+	<button class="btn btn-primary">View</button>
+	<button class="btn btn-info">Modify</button>
+</div><div id="ProductDisplay">
+	<img src="../images/1.jpg" class="img-rounded">
+	<button class="btn btn-primary">View</button>
+	<button class="btn btn-info">Modify</button>
+</div><div id="ProductDisplay">
+	<img src="../images/1.jpg" class="img-rounded">
+	<button class="btn btn-primary">View</button>
+	<button class="btn btn-info">Modify</button>
+</div><div id="ProductDisplay">
+	<img src="../images/1.jpg" class="img-rounded">
+	<button class="btn btn-primary">View</button>
+	<button class="btn btn-info">Modify</button>
+</div><div id="ProductDisplay">
+	<img src="../images/1.jpg" class="img-rounded">
+	<button class="btn btn-primary">View</button>
+	<button class="btn btn-info">Modify</button>
+</div><div id="ProductDisplay">
+	<img src="../images/1.jpg" class="img-rounded">
+	<button class="btn btn-primary">View</button>
+	<button class="btn btn-info">Modify</button>
+</div><div id="ProductDisplay">
+	<img src="../images/1.jpg" class="img-rounded">
+	<button class="btn btn-primary">View</button>
+	<button class="btn btn-info">Modify</button>
+</div><div id="ProductDisplay">
+	<img src="../images/1.jpg" class="img-rounded">
+	<button class="btn btn-primary">View</button>
+	<button class="btn btn-info">Modify</button>
+</div><div id="ProductDisplay">
+	<img src="../images/1.jpg" class="img-rounded">
+	<button class="btn btn-primary">View</button>
+	<button class="btn btn-info">Modify</button>
+</div><div id="ProductDisplay">
+	<img src="../images/1.jpg" class="img-rounded">
+	<button class="btn btn-primary">View</button>
+	<button class="btn btn-info">Modify</button>
+</div><div id="ProductDisplay">
+	<img src="../images/1.jpg" class="img-rounded">
+	<button class="btn btn-primary">View</button>
+	<button class="btn btn-info">Modify</button>
+</div><div id="ProductDisplay">
+	<img src="../images/1.jpg" class="img-rounded">
+	<button class="btn btn-primary">View</button>
+	<button class="btn btn-info">Modify</button>
+</div><div id="ProductDisplay">
+	<img src="../images/1.jpg" class="img-rounded">
+	<button class="btn btn-primary">View</button>
+	<button class="btn btn-info">Modify</button>
 </div>
 
 
