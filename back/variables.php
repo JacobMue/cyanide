d461f7b2bcd7b187352bc30ebaef8b0297c82369
Jacob Mue <muejacob@gmail.comm>
2016-05-19 18:03:42 +0300

    the same files mike uploaded just from my side

--- back/variables.php
+++ back/variables.php
@@ -1,8 +1,35 @@
 <?php
 
+
+$environment=$_SERVER['SERVER_NAME'];
 $server="localhost";
-$user="83823";
-$password="ThasTup8";
-$database="cyanide";
+if($environment=="localhost"){
+  $user="root";
+  $password="";
+  $database="cyanide";  
+}
+else 
+{
+   $user="83823";
+   $password="ThasTup8";
+   $database="db83823";
+    
+}
+
+
+$con=mysql_connect($server,$user,$password,$database) or die("NO server and database");
+
 
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
