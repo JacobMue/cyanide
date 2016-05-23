c183a297aeb8e7cc3fc1e877952ee781006d03c3
Jacob Mue <muejacob@gmail.comm>
2016-05-23 21:51:55 +0300

    Update DisplayProduct.php

--- back/DisplayProduct.php
+++ back/DisplayProduct.php
@@ -3,7 +3,13 @@ $category="";
 if(isset($_GET['category'])){
 	$category=$_GET['category'];
 }
-$sql="SELECT * FROM `product` WHERE `CategoryName`='$category'";
+
+$con=mysqli_connect("localhost","root","","cyanide");
+if($con->connect_error){
+	echo $con->connect_error;
+}else{
+
+$sql="SELECT * FROM `product` WHERE `CategoryName`='$category' AND `active`=1";
 $query=$con->query($sql);
 $row=mysqli_num_rows($query);
 
@@ -26,7 +32,7 @@ else{
 }
 $con->close();	
 
-
+}
 
 ?>
 
